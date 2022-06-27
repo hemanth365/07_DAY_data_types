4bc3e11d31395301bf64f44d0aeb70e4add712ae
Hemanth Krishna Nanda <107603676+hemanth365@users.noreply.github.com>
2022-06-27 18:47:16 +0530

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,12 @@
+print(type('hello world'))
+print(type(5648))
+print(type('PYTHON'))
+print(type(33.45))
+print(type('154.36'))
+print(type('3335468595'))
+print(type('EASY_PEASY'))
+print(type('HIII+HEY'))
+print(type(125468.99))
+print(type(4587))
+
+'''note: only int, float, string are shown (or) completed'''
